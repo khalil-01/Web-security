e66ecd047a4dc1f4d78623de29c8706bb9294e48
khalil-01 <121757275+khalil-01@users.noreply.github.com>
2025-03-25 13:20:14 +0200

    Add files via upload

---
+++ cross site scripting/register_logic_home.php
@@ -0,0 +1,21 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database_name = "test1";
+
+$conn = new mysqli($host, $username, $password, $database_name);
+
+if (isset($_POST['login'])) {
+    $email_form = $_POST['email'];
+    $password_form = $_POST['password'];
+
+    $sql_select = "SELECT * FROM users WHERE email = '$email_form'";
+    $result = $conn->query($sql_select);
+    if ($result->num_rows > 0) {
+        $hashed_password = $result->fetch_assoc()['password'];
+        if (password_verify($password_form, $hashed_password)) {
+            echo "login data true";
+        }
+    }
+}
